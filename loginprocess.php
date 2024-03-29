fbd6c8e312ce0948c32f5e99acbc299d7731890e
programmer647 <bourne.scm@oundleschool.org.uk>
2024-03-29 16:08:01 +0000

    login page completed

---
+++ loginprocess.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+//print_r($_POST);
+include_once ("connection.php");
+array_map("htmlspecialchars", $_POST);
+$stmt = $conn->prepare("SELECT * FROM tblusers WHERE Username=:Username;" );
+$stmt->bindParam(':Username', $_POST['Username']);
+$stmt->execute();
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{ 
+    $hashed = $row['Password'];
+    $attempt= $_POST['Pword'];
+    if(password_verify($attempt, $hashed)){
+        $_SESSION['name']=$row['Username'];
+        $_SESSION['role']=$row['Role'];
+        $_SESSION['id']=$row['UserID'];
+        echo("Logged in");
+    }else{
+        echo("Incorrect password");
+        //header('Location: login.php');
+    }
+}
+$conn=null;
+?>
