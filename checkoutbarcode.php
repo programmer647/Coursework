d144d35dcb3d89404595144f8b937dbf46f9b5a8
programmer647 <bourne.scm@oundleschool.org.uk>
2024-04-24 08:59:30 +0100

    changes

---
+++ checkoutbarcode.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Checkout</title>
+    
+</head>
+<body>
+
+
+<?php
+session_start(); 
+?>
+
+      
+<form action="addtobasket.php" method="post"><!--Creates the form where the information can be entered. Also sets the page to submit 
+the information to and the way to do it which is through the post function. -->
+  Scan barcode:<input type="text" name="barcode"><br><!--Provides a box to input the barcode-->
+  <input type ="submit" value="Add to basket"><!--Allows the information to be submitted by providing a button to do it-->
+</form>
+
+
+<form action="
+
+
+</body>
+</html>
+
+
+
