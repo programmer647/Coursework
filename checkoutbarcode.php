df96fb682264ed27014f079586117a80742c6f75
programmer647 <bourne.scm@oundleschool.org.uk>
2024-05-11 08:37:33 +0100

    barcode page finished

--- checkoutbarcode.php
+++ checkoutbarcode.php
@@ -19,6 +19,10 @@ the information to and the way to do it which is through the post function. -->
   <input type ="submit" value="Add to basket"><!--Allows the information to be submitted by providing a button to do it-->
 </form>
 
+<form action="checkoutmanual.php" method="post"><!--sets the page to redirect to when the button is pressed-->
+  <input type="submit" value="Or enter details manually"><!--Creates a button to redirect to the manual checkout page-->
+</form>
+
 
 
 
