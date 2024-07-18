09b967466a2adb144b53bde8d2351f2ce66388f6
programmer647 <bourne.scm@oundleschool.org.uk>
2024-07-18 12:54:04 +0100

    home page

--- loggedouthome.php
+++ loggedouthome.php
@@ -4,15 +4,19 @@
 <head>
     
     <title>Home Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"><!--links to the bootstrap -->
+    <link rel="stylesheet" href="style.css"><!--links to the external style sheet-->
 
 </head>
 <body>
 
-
+<!--creating the navbar-->
 <nav class="navbar navbar-default">
         <div class="container-fluid">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="loggedouthome.php">Home</a></li>
+            <li class="active"><a href="loggedouthome.php">Home</a></li><!--sets the home page to the active link so that it appears a different 
+            colour so that the user knows which page they are currently on-->
+            <!--the code below provides the links to the different pages-->
             <li><a href="about.php">About Us</a></li>
             <li><a href="shop.php">Shop</a></li>
             <li><a href="news.php">News</a></li>
