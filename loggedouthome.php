4a014bc044ac77fea8f0cd12ba55f738acfa4486
programmer647 <bourne.scm@oundleschool.org.uk>
2024-07-18 12:21:34 +0100

    navbar on home page

---
+++ loggedouthome.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+        
+<head>
+    
+    <title>Home Page</title>
+
+</head>
+<body>
+
+
+<nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="loggedouthome.php">Home</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="shop.php">Shop</a></li>
+            <li><a href="news.php">News</a></li>
+            <li><a href="faqs.php">FAQs</a></li>
+            <li><a href="uniformlists.php">Uniform Lists</a></li>
+            <li><a href="login.php">Login/Sign up</a></li>
+
+          </ul>
+        </div>
+      </nav>
+
+</body>
+</html>
