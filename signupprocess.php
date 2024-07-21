dddc1ea5f6a3f66f8ddab9530a2636490df05f9d
programmer647 <bourne.scm@oundleschool.org.uk>
2024-07-21 21:26:41 +0100

    sign up page

--- signupprocess.php
+++ signupprocess.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-include_once("connection.php");
+include_once("connection.php");//allows the page to connect to the database
 
-print_r($_POST);
-
-if ($_POST['password']!=$_POST['confirm']){
-    echo "<script>alert('Confirm password not the same as password entered. Please try again.');</script>";
-    header("location:signup.php");
+if ($_POST['password']!=$_POST['confirm']){//checks if the password and confirm password entered were the same
+    echo "<script>alert('Confirm password not the same as password entered. Please try again.');</script>";//alerts the user to say that 
+    //the passwords weren't the same
 }
 
 ?>
+
+
