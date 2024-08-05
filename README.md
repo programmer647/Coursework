18d1e60e2a1d8879b2c8ecfcfe18803aa294117f
programmer647 <bourne.scm@oundleschool.org.uk>
2024-08-05 13:38:36 +0100

    changes to sign up page

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 
 Currently doing:
-finish sign up page so that it adds to the database
+finish sign up page so that it adds to the database - currently doing validation
 
 
 Next steps:
@@ -22,6 +22,7 @@ maybe change hsm name and matron name in table to their email addresses as names
 might need to get rid of houses table
 if I get rid of houses table take new screenshots of tables which depend on it and then put them into the word document - i have already edited the code but the screenshots and description of the table would need to be changed
 maybe add pop up button to complete page in case someone presses it by accident
+need to fix CSS so that when the page is made smaller it still looks ok
 
 
 
--- signupprocess.php
+++ signupprocess.php
@@ -2,7 +2,11 @@
 session_start();
 include_once("connection.php");//allows the page to connect to the database
 
-if ($_POST['password']!=$_POST['confirm']){ ?> <!--checks if the password and confirm password entered were the same-->
+$valid=TRUE;
+
+if ($_POST['password']!=$_POST['confirm']){ //checks if the password and confirm password entered were the same
+    $valid=FALSE;
+    ?>
     <body>
         <h1>Confirm password is not the same as password entered. Please try again<h1>
         <form action="signup.php"><!--sets the page to redirect back to so that the user can try again-->
@@ -18,6 +22,7 @@ $stmt->execute();
 while ($row=$stmt->fetch(PDO::FETCH_ASSOC))
     {
         if (($row['username'])==($_POST['username'])){//checks if the username entered is the same as one in the table
+            $valid=FALSE;
             ?>
             <h1>Username already in use. Try another one.</h1>
             <form action="signup.php"><!--sets the page to redirect back to so that the user can try again-->
@@ -27,6 +32,20 @@ while ($row=$stmt->fetch(PDO::FETCH_ASSOC))
         }
     }
 
+
+if (($_POST['forename'])=="" or ($_POST['surname'])=="" or ($_POST['username'])=="" or ($_POST['password'])=="") {
+    ?>
+    <h1>Please enter all details</h1>
+    <form action="signup.php"><!--sets the page to redirect back to so that the user can try again-->
+        <input type="submit" value="Try again"><!--Creates a button to allow the user to return to the sign in page-->
+</form>
+<?php
+}
+
+
+if ($valid==TRUE){
+    $stmt=$conn->prepare("INSERT INTO Tblusers (UserID,Forename,Surname,Username,Password,Role) VALUES (null,:forename,:surname,:username,:password,:role") 
+}
 
 
 ?>
