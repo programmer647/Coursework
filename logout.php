dd1b105eb409bcb638ed96d6d352b417f89835e2
programmer647 <bourne.scm@oundleschool.org.uk>
2024-08-23 15:17:14 +0100

    log out css and boostrap started

--- logout.php
+++ logout.php
@@ -1,15 +1,57 @@
-<?php
-session_start();
+<!DOCTYPE html><!--Declares that the document is a HTML page-->
+<html>
+<head>
+<title>Login</title><!--sets the title of the page-->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"><!--links to the bootstrap -->
+<link rel="stylesheet" href="style.css"><!--links to the external style sheet-->
+
+<nav class="navbar navbar-fixed-top"><!--fixes the navbar to the top of the page-->
+        <div class="container-fluid">
+          <ul class="nav navbar-nav">
+            <li><a href="customerhome.php">Home</a></li><!--sets the home page to the active link so that it appears a different 
+            colour so that the user knows which page they are currently on-->
+            <!--the code below provides the links to the different pages-->
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="shop.php">Shop</a></li>
+            <li><a href="news.php">News</a></li>
+            <li><a href="faqs.php">FAQs</a></li>
+            <li><a href="uniformlists.php">Uniform Lists</a></li>
+            <li class="active"><a href="login.php">Login/sign up</a></li>
+
+          </ul>
+        </div>
+      </nav>
+
+
+</head>
+
+<body><!--Starts the body section of the page-->
+
+<div class="container-fluid">
+  <img src="images/shop1.jpg" style="width:100%;">
+  <div class="centered box white">
+  <?php
+
+print_r($_SESSION);
 if(isset($_SESSION['name']))//checks if there is a user logged in
 {
     unset($_SESSION['name']);//unsets the session variable name which stores the user's username
     unset($_SESSION['role']);//unsets the session variable which stores the user's role
-    echo("Logged out successfully");
+    echo("<h3>Logged out successfully</h3>");
 }
 else{
-    echo("No user logged in");
+    echo("<h3>No user logged in</h3>");
 }
 
 
 ?>
 
+  
+
+</div>
+</div>
+
+</body><!--Ends the body section of the page-->
+
+</html><!--Marks the end of the document-->
