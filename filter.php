5ad259c134cfa2316454189c85cdbe1fe05a6ffe
programmer647 <bourne.scm@oundleschool.org.uk>
2024-10-03 12:00:08 +0100

    work on filtering system

--- filter.php
+++ filter.php
@@ -4,22 +4,76 @@ session_start();
 
 print_r($_POST);
 
-if ($_POST['Uniform']=="Alluniform" AND $_POST['Type']=="Alltypes" AND $_POST['Year']=="All"){
+if ($_POST['Uniform']=="Alluniform" AND $_POST['Type']=="Alltypes" AND $_POST['Year']=="Allyears"){
+    echo("Yes");
     $stmt=$conn->prepare("SELECT CategoryID from TblCategories");
+    //$stmt->execute();
 }
 
-if ($_POST['Uniform']=="Alluniform" AND $_POST['Type']=="Alltypes" AND $_POST['Year']=="middle"){
+elseif($_POST['Uniform']=="Alluniform" AND $_POST['Type']=="Alltypes" AND $_POST['Year']=="middle"){
+    $stmt=$conn->prepare("SELECT CategoryID from TblCategories where Year=:year");
+    $stmt->bindParam(':year',"All");
+    //$stmt->execute();
+}
+
+elseif($_POST['Uniform']=="Alluniform" AND $_POST['Type']=="Alltypes" AND ($_POST['Year']=="Junior" OR $_POST['Year']=="Sixth")){
+    $stmt=$conn->prepare("SELECT CategoryID from TblCategories where Year=:year");
+    $stmt->bindParam(':year',$_POST['Year']);
+    //$stmt->execute();
+}
+
+elseif($_POST['Uniform']=="Alluniform" AND $_POST['Year']=="Allyears"){
+    $stmt=$conn->prepare("SELECT CategoryID from TblCategories where Type=:type");
+    $stmt->bindParam(':type',$_POST['Type']);
+    //$stmt->execute();
+}
+
+elseif($_POST['Type']=="Alltypes" AND $_POST['Year']=="Allyears"){
+    echo("yes");
+    $stmt=$conn->prepare("SELECT CategoryID from TblCategories where Uniform=:uniform");
+    $stmt->bindParam(':uniform',$_POST['Uniform']);
+    //$stmt->execute();
+}
+
+
+elseif($_POST['Year']=="Allyears"){
+    $stmt=$conn->prepare("SELECT CategoryID from TblCategories where Uniform=:uniform and Type=:type");
+    $stmt->bindParam(':uniform',$_POST['Uniform']);
+    $stmt->bindParam(':type',$_POST['Type']);
+    //$stmt->execute();
+}
+
+elseif($_POST['Type']=="Alltypes"){
+    $stmt=$conn->prepare("SELECT CategoryID from TblCategories where Uniform=:uniform and Year=:year");
+    $stmt->bindParam(':uniform',$_POST['Uniform']);
+    $stmt->bindParam(':year',$_POST['Year']);
+    //$stmt->execute();
+}
+
+elseif($_POST['Uniform']=="Alluniform"){
+    $stmt=$conn->prepare("SELECT CategoryID from TblCategories where Type=:type and Year=:year");
+    $stmt->bindParam(':type',$_POST['Type']);
+    $stmt->bindParam(':year',$_POST['Year']);
+    //$stmt->execute();
+}
+
+else{
     $stmt=$conn->prepare("SELECT CategoryID from TblCategories where Uniform=:uniform and Type=:type and Year=:year");
+    $stmt->bindparam(':uniform',$_POST['Uniform']);
+    $stmt->bindparam(':type',$_POST['Type']);
+    $stmt->bindparam(':year',$_POST['Year']);
+    //$stmt->execute();
 }
 
-
-$stmt=$conn->prepare("SELECT CategoryID from TblCategories where Uniform=:uniform and Type=:type and Year=:year");
-$stmt->bindparam(':uniform',$_POST['Uniform']);
-$stmt->bindparam(':type',$_POST['Type']);
-$stmt->bindparam(':year',$_POST['Year']);
 $stmt->execute();
+while ($row=$stmt->fetch(PDO::FETCH_ASSOC))
+{
+    print_r($row);
+}
 
 
 //create an array of the possible categories, send this back to the page and then only display items from these categories
 
 ?>
+
+
--- shop.php
+++ shop.php
@@ -12,6 +12,8 @@ session_start();//starts the session so that session variables can be accessed
     <title>Shop</title><!--sets the title of the page-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"><!--links to the bootstrap -->
     <link rel="stylesheet" href="style.css"/><!--links to the external style sheet-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 </head>
 
@@ -43,13 +45,46 @@ session_start();//starts the session so that session variables can be accessed
       <body>
 
 
-<div class="sort">
-    <button type="button" class="btn btn-info">Sort</button>
-</div>
+<div class="row">
+    <div class="col-sm-2"></div>
+    <div class="dropdown">
+    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#filterDropdown">Filter</button>
+        <div id="filterDropdown" class="collapse">
+        <form action="filter.php" method="POST">
+            <p>Uniform</p>
+            <input type="radio" id="c" name="Uniform" value="Culotte">
+            <label for="c">Culotte</label><br>
+            <input type="radio" value="Trouser" name="Uniform" id="t">
+            <label for="t">Trouser</label><br>
+            <input type="radio" value="Unisex" name="Uniform" id="u">
+            <label for="u">Unisex</label><br>
+            <input type="radio" value="Alluniform" name="Uniform" id="au">
+            <label for="au">All</label><br>
 
+            <p>Sports/uniform</p>
+            <input type="radio" id="school" value="School" name="Type">
+            <label for="school">Schoolwear</label><br>
+            <input type="radio" id="sport" value="Sport" name="Type">
+            <label for="sport">Sportswear</label><br>
+            <input type="radio" value="Alltypes" name="Type" id="at">
+            <label for="at">All</label><br>
 
+            <p>Year Group</p>
+            <input type="radio" id="Junior" name="Year" value="Junior">
+            <label for="junior">1st and 2nd form</label><br>
+            <input type="radio" value="middle" name="Year" id="Middle">
+            <label for="Middle">3rd - 5th form</label><br>
+            <input type="radio" value="Sixth" name="Year" id="Senior">
+            <label for="Senior">6th form</label><br>
+            <input type="radio" value="Allyears" name="Year" id="ay">
+            <label for="ay">All</label><br>
 
+            <input type="submit" value="Apply">
 
+        </form>
+        </div>
+        </div>
+        </div>
 
 <div class="container-fluid">
 <?php
@@ -77,15 +112,18 @@ while ($row=$stmt->fetch(PDO::FETCH_ASSOC)){
 }
 
 ?>
-
 </div>
 
 
-<script>
 
+<script>
+    function filterDrop(){
+        document.getElementById("filterDropdown").style.display = "none";
+
+    }
 
 </script>
 
-
 </body>
+
 </html>
