6e37f8dc4779c8813c1f809922b855d8bd8c5f55
programmer647 <bourne.scm@oundleschool.org.uk>
2024-10-18 15:33:19 +0100

    getprice page finished

--- getprice.php
+++ getprice.php
@@ -1,13 +1,12 @@
 <?php
 
 include_once("connection.php");
-print_r($_GET);
 $stmt=$conn->prepare("SELECT Price FROM Tbltype WHERE TypeID=:id");
 $stmt->bindparam(":id", $_GET['q']);
 $stmt->execute();
 //$row=$stmt->fetch(PDO::FETCH_ASSOC);
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    print_r($row);
+    echo("£".$row['Price']);
   }
 
 //echo($row['Price']);
