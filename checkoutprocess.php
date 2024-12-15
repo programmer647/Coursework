b5b7413a71531961005f85897231a66a2d2063fe
programmer647 <bourne.scm@oundleschool.org.uk>
2024-12-15 17:15:08 +0000

    changes

--- checkoutprocess.php
+++ checkoutprocess.php
@@ -6,7 +6,7 @@ session_start();
 $method=$_SESSION['method'];
 
 if ($method=="home"){
-    $stmt=$conn->prepare("UPDATE Tblorders SET AddressLine1=:address1, AddressLine2=:address2, Postcode=:postcode WHERE OrderID=:orderid");
+    $stmt=$conn->prepare("UPDATE Tblorders SET AddressLine1=:address1, AddressLine2=:address2, Postcode=:postcode, Usercompleted=1 WHERE OrderID=:orderid");
     $stmt->bindParam(':address1', $_POST['addressline1']);
     $stmt->bindParam(':address2',$_POST['addressline2']);
     $stmt->bindParam(':postcode',$_POST['postcode']);
@@ -16,14 +16,14 @@ if ($method=="home"){
 
 elseif ($method=="collect"){
     echo('yes');
-    $stmt=$conn->prepare("UPDATE Tblorders SET Email=:email WHERE OrderID=:orderid");
+    $stmt=$conn->prepare("UPDATE Tblorders SET Email=:email, Usercompleted=1 WHERE OrderID=:orderid");
     $stmt->bindParam(':email', $_POST['email']);
     $stmt->bindParam(':orderid', $_SESSION['orderid']);
     $stmt->execute();
 }
 
 elseif($method="boarding"){
-    $stmt=$conn->prepare("UPDATE Tblorders SET Pupilname=:name, Year=:year, Tutor=:Tutor, House=:house WHERE OrderID=:orderid");
+    $stmt=$conn->prepare("UPDATE Tblorders SET Pupilname=:name, Year=:year, Tutor=:Tutor, House=:house, Usercompleted=1 WHERE OrderID=:orderid");
     $stmt->bindParam(':name', $_POST['name']);
     $stmt->bindParam(':year',$_POST['year']);
     $stmt->bindParam(':tutor',$_POST['tutor']);
@@ -36,6 +36,6 @@ elseif($method="boarding"){
 unset($_SESSION['orderid']);
 unset($_SESSION['method']);
 
-print_r($_SESSION);
+
 
 ?>
