80d2f678be397929731b235291d1ef9de1926dd6
programmer647 <bourne.scm@oundleschool.org.uk>
2024-12-15 16:40:36 +0000

    checkoutprocess finsiehd

--- checkoutprocess.php
+++ checkoutprocess.php
@@ -2,9 +2,6 @@
 include_once("connection.php");
 session_start();
 
-print_r($_SESSION);
-
-print_r($_POST);
 
 $method=$_SESSION['method'];
 
@@ -18,15 +15,27 @@ if ($method=="home"){
 }
 
 elseif ($method=="collect"){
-    
+    echo('yes');
+    $stmt=$conn->prepare("UPDATE Tblorders SET Email=:email WHERE OrderID=:orderid");
+    $stmt->bindParam(':email', $_POST['email']);
+    $stmt->bindParam(':orderid', $_SESSION['orderid']);
+    $stmt->execute();
 }
 
 elseif($method="boarding"){
-    ?>
-    <form action="checkoutprocess.php" method="POST">
-        Pupil name:<input type="text" name="name"><br>
-        Pupil year group:<input type="text" name="year"><br>
-        Pupil's tutor:<input type="text" name="tutor"><br>
-        Boarding house:<input type="text" name="house"><br>
-    <?php
+    $stmt=$conn->prepare("UPDATE Tblorders SET Pupilname=:name, Year=:year, Tutor=:Tutor, House=:house WHERE OrderID=:orderid");
+    $stmt->bindParam(':name', $_POST['name']);
+    $stmt->bindParam(':year',$_POST['year']);
+    $stmt->bindParam(':tutor',$_POST['tutor']);
+    $stmt->bindParam(':house',$_POST['house']);
+    $stmt->bindParam(':orderid', $_SESSION['orderid']);
+    $stmt->execute();
 }
+
+
+unset($_SESSION['orderid']);
+unset($_SESSION['method']);
+
+print_r($_SESSION);
+
+?>
