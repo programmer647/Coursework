e19dfb55cc831b8589b9716dcc4c63c59085b7a5
programmer647 <bourne.scm@oundleschool.org.uk>
2024-12-21 17:46:03 +0000

    issue with submit button

---
+++ detailedorder.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once("connection.php");
+session_start();
+
+$id=$_GET['id'];
+
+$ordernums=[];
+
+
+$stmt=$conn->prepare("SELECT tblitems.ItemID as i, Name as n, Size as s, Quantity as q FROM Tblonlineorders 
+    INNER JOIN tblorders ON tblonlineorders.OrderID=tblorders.OrderID
+    INNER JOIN tbltype ON tblonlineorders.TypeID=tbltype.TypeID
+    INNER JOIN tblitems ON tbltype.ItemID=tblitems.ItemID
+    WHERE tblonlineorders.OrderID=:orderid");
+    $stmt->bindParam(':orderid',$id);
+    $stmt->execute();
+    while ($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+            $ordernums[$row['i']]["Name"]=$row['n'];
+            $ordernums[$row['i']]["Size"]=$row['s'];
+            $ordernums[$row['i']]["Quantity"]=$row['q'];
+        }
+    
+?>
+
+<form action="readycollect.php" method="POST">
+
+<?php
+$i=0;
+foreach($ordernums as $x){
+    for ($j=1; $j<=$x['Quantity'];$j++){
+        echo($x['Name'].' '.$x['Size']);
+        echo("<input type='number' name=$i>");
+        echo("<br>");
+        $i=$i+1;
+    }
+}
+
+echo("<input type='submit' value='Submit'");
+
+?>
+
+</form>
+
+
+
