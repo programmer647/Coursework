632341cad0898d24361a53d3c2f6c8a52b271b42
programmer647 <bourne.scm@oundleschool.org.uk>
2024-12-27 17:38:19 +0000

    progress on checkout system

--- addtobasket.php
+++ addtobasket.php
@@ -3,4 +3,25 @@ session_start();
 include_once("connection.php");
 print_r($_POST);
 
+$date = date('Y-m-d');
+
+if (!isset($_SESSION['orderid']))
+{
+    $stmt=$conn->prepare("INSERT INTO Tblorders(UserID,Datecreated,Datecompleted) Values(:userid,:datecreated,:datecompleted)");
+    $stmt->bindParam(':userid',$_SESSION['id']);
+    $stmt->bindParam(':datecreated',$date);
+    $stmt->bindParam(':datecompleted',$date);
+    $stmt->execute();
+    
+    $stmt=$conn->prepare("SELECT MAX(OrderID) from Tblorders");
+    $stmt->execute();
+    while ($row=$stmt->fetch(PDO::FETCH_ASSOC))
+    {
+    $_SESSION['orderid']=$row['MAX(OrderID)'];
+    }
+}
+
 ?>
+
+
+
