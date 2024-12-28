4db69977f51811e3632dfceb38b6a8acd2602184
programmer647 <bourne.scm@oundleschool.org.uk>
2024-12-28 18:21:30 +0000

    work on show page

--- show.php
+++ show.php
@@ -1,4 +1,11 @@
 <?php
 include_once("connection.php");
 
+$stmt=$conn->prepare("SELECT * FROM Tblhouse");
+$stmt->execute();
+while ($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+  $housetotals[$row['HouseID']]
+}
+
 ?>
+
