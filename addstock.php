0f65da1e29203a390c7cab7448ccc685b18bb817
programmer647 <bourne.scm@oundleschool.org.uk>
2025-01-19 20:55:33 +0000

    changes

--- addstock.php
+++ addstock.php
@@ -1,5 +1,32 @@
 <?php
+include_once("connection.php");//connects the page to the database
+session_start();//allows the page to access the session variables
+
+//this code prevents unathorised users from access the page
+if ($_SESSION['role']==1)//checks if the user is a customer
+{   
+    header("Location:customerhome.php");//if the user is a customer not a volunteer or admin they are redirected to the home page
+}
+
+header("Location: stock.php");
 session_start();
-include_once("connection.php");
+
+
+//selects the stock from the uniform table where the UniformID matches the barcode scanned
+$stmt=$conn->prepare("SELECT Stock FROM tbluniform where UniformID=:id");
+$stmt->bindParam(':id',$_POST['barcode']);
+$stmt->execute();
+$row=$stmt->fetch(PDO::FETCH_ASSOC);
+$stock=$row['Stock'];
+
+$stock=$stock+1;//adds one to the stock
+
+//sets the stock of the uniform item to the new stock variable which is the original stock plus 1
+$stmt=$conn->prepare("UPDATE Tbluniform SET Stock=:stock WHERE UniformID=:id");
+$stmt->bindParam(':stock',$stock);
+$stmt->bindParam(':id',$_POST['barcode']);
+$stmt->execute();
 
 ?>
+
+
--- stock.php
+++ stock.php
@@ -1,7 +1,19 @@
 <?php
-include_once("connection.php");
-session_start();
+session_start();//allows the page to access the session variables
+
+//this code prevents unathorised users from access the page
+if ($_SESSION['role']==1)//checks if the user is a customer
+{   
+    header("Location:customerhome.php");//if the user is a customer not a volunteer or admin they are redirected to the home page
+}
+
 ?>
 
-<form action="addstock.php" method="POST">
-    <input type=
+<form action="addstock.php" method="POST"><!--creates a form where the barcode can be entered. When the form is submitted the data is sent to 
+the next page using the post method-->
+<label for="barcode">Scan barcode:</label><!--adds a label to the barcode input-->
+<input type="number" name="barcode"><br><!--creates an input field for the barcode-->
+<input type="submit" value="Add to stock"><!--creates a button so that the form can be submitted-->
+
+
+
